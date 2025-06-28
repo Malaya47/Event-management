14d0d164a813fe850cef4f3c65df9c512ec6b30b
Malaya Tiwari <sidzilegend@gmail.com>
2025-06-28 11:35:54 +0530

    update: made login API

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,12 @@ const express = require("express");
 const authRouter = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const { validateSignupData } = require("../utils/validations");
+const {
+  validateSignupData,
+  validateLoginData,
+} = require("../utils/validations");
 
 authRouter.post("/signup", async (req, res) => {
   try {
@@ -27,6 +31,39 @@ authRouter.post("/signup", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "An error occured while registering user",
+      error: error.message,
+    });
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    // sanitize and validate data received
+    validateLoginData(req);
+    const { email, password } = req.body;
+    // check if user exsists
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+
+    // check if password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      const token = jwt.sign({ _id: user._id }, "your_jwt_secret", {
+        expiresIn: "1d",
+      });
+
+      res.status(200).json({
+        message: "Login successful",
+        token,
+        user: { _id: user._id, name: user.name, email: user.email },
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occured while logging in",
       error: error.message,
     });
   }
--- backend/utils/validations.js
+++ backend/utils/validations.js
@@ -22,6 +22,35 @@ const validateSignupData = (req) => {
   }
 };
 
+const validateLoginData = (req) => {
+  const allowedFields = ["email", "password"];
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new Error("Email and password are required");
+  }
+
+  const isAllowed = Object.keys(req.body).every((key) =>
+    allowedFields.includes(key)
+  );
+
+  if (!isAllowed) {
+    throw new Error("Invalid fields");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Email is invalid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      "Password should have at least one uppercase letter, one lowercase letter, one number and one special character"
+    );
+  }
+};
+
 module.exports = {
   validateSignupData,
+  validateLoginData,
 };
